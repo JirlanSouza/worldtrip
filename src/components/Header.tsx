1a85701a3db2fd2738e5a11a1e189b30aba961fe
Jirlan <jirlansouza08@gmaial.com>
2022-07-03 20:48:27 -0400

    Chaged the Header component to added the responsive styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon, IconButton, Img } from "@chakra-ui/react";
+import { Box, Flex, Icon, IconButton, Img } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { RiArrowLeftSLine } from "react-icons/ri";
 
@@ -6,28 +6,38 @@ export function Header() {
   const router = useRouter();
 
   return (
-    <Flex
-      as="header"
-      height={50}
-      maxWidth={1440}
-      align="center"
-      justify="center"
-      position="relative"
-    >
-      {router.asPath !== "/" && (
-        <IconButton
-          aria-label="return"
-          variant="unstyled"
-          icon={<Icon as={RiArrowLeftSLine} color="dark.text" />}
-          position="absolute"
-          left="2"
-          onClick={() => {
-            router.back();
-          }}
-        />
-      )}
+    <Box as="header">
+      <Flex
+        height={[50, 62, 100]}
+        maxWidth={1440}
+        marginLeft="auto"
+        marginRight="auto"
+        align="center"
+        justify="center"
+        position="relative"
+      >
+        {router.asPath !== "/" && (
+          <IconButton
+            aria-label="return"
+            variant="unstyled"
+            size={["xs", "md", "lg"]}
+            icon={
+              <Icon
+                as={RiArrowLeftSLine}
+                fontSize={[16, 24, 32]}
+                color="dark.text"
+              />
+            }
+            position="absolute"
+            left="2"
+            onClick={() => {
+              router.back();
+            }}
+          />
+        )}
 
-      <Img src="/logo.svg" width={82} />
-    </Flex>
+        <Img src="/logo.svg" width={[82, 120, 190]} />
+      </Flex>
+    </Box>
   );
 }
